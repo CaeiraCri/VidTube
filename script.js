31d6df4585ac146a7ad7a0ed0d6e15086b1d0630
Cristian Caeira <caeiracri@gmail.com>
2023-02-19 18:04:52 +0100

    v2

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector('.menu-icon');
+const sidebar = document.querySelector('.sidebar');
+const container = document.querySelector('.container');
+
+menuIcon.addEventListener('click', () => {
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("big-container");
+});
